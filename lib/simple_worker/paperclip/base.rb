5d82d241e2d155cc7c838c361e29f2bf37cb75fe
Luke Carpenter <luke.carpenter@appoxy.com>
2011-07-09 23:28:02 +0100

    Method for answering "Is SimpleWorker configured correctly?"

--- lib/simple_worker/paperclip/base.rb
+++ lib/simple_worker/paperclip/base.rb
@@ -3,7 +3,11 @@ module SimpleWorker
   module Paperclip
     # Class for de-facto's, such as checking for configuration
     class Base
-
+      # Method for checking if SimpleWorker has been properly configured to run Paperclip in the cloud
+      # @returns true
+      def self.configured?
+        SimpleWorker.config.auto_merge == true && SimpleWorker.config.database.is_a?(Hash) && SimpleWorker.config.arbitary[:paperclip] == true
+      end
     end
   end
 end
